e13554fd1c310a3b50d288221e1fcff0ff039403
Sun-ZhenXing <1006925066@qq.com>
2022-12-27 17:49:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vuepress 算法和数据结构笔记
+
+![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/Sun-ZhenXing/vuepress-data-structure/deploy-docs.yml?branch=main)
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,56 @@
+import { defineUserConfig, defaultTheme } from 'vuepress'
+import { mdEnhancePlugin } from 'vuepress-plugin-md-enhance'
+import { copyCodePlugin } from 'vuepress-plugin-copy-code2'
+import { searchProPlugin } from 'vuepress-plugin-search-pro'
+
+const USER_NAME = 'Sun-ZhenXing'
+const BASE_PATH = '/vuepress-data-structure/'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '算法和数据结构笔记',
+  description: '算法和数据结构笔记',
+  head: [
+    ['link', { rel: 'icon', href: `${BASE_PATH}favicon.svg` }]
+  ],
+  base: BASE_PATH,
+  markdown: {
+    code: {
+      lineNumbers: 10
+    }
+  },
+  theme: defaultTheme({
+    logo: '/favicon.svg',
+    repo: `${USER_NAME}${BASE_PATH}`,
+    docsDir: 'docs',
+    navbar: [
+    ],
+    sidebar: {
+    }
+  }),
+  plugins: [
+    mdEnhancePlugin({
+      gfm: true,
+      container: true,
+      linkCheck: true,
+      vPre: true,
+      tabs: true,
+      codetabs: true,
+      align: true,
+      attrs: true,
+      sub: true,
+      sup: true,
+      footnote: true,
+      mark: true,
+      imageLazyload: true,
+      tasklist: true,
+      katex: true,
+      mermaid: true,
+      delay: 200,
+    }),
+    searchProPlugin({}),
+    copyCodePlugin({
+      showInMobile: true,
+    }),
+  ]
+})
